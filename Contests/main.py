8ed1d303ae8078729d0593305d92d5d14f07a74d
A Sergey <agibalov.sd@phystech.edu>
2021-09-20 01:31:22 +0300

    6rd exercise completed

--- Contests/main.py
+++ Contests/main.py
@@ -1,8 +1,2 @@
-def Mnogobonachi(n):
-    if 1 <= n <= 8:
-        return 0
-    elif n == 9:
-        return 1
-    elif n > 9:
-        return Mnogobonachi(n - 1) + Mnogobonachi(n - 2) + Mnogobonachi(n - 3) + Mnogobonachi(n - 4) + Mnogobonachi(n - 5) + Mnogobonachi(n - 6) + Mnogobonachi(n - 7) + Mnogobonachi(n - 8) + Mnogobonachi(n - 9)
-print(Mnogobonachi(int(input())))
+with open(input(), 'r') as f:
+    print(max(f.read().split(), key=len))
