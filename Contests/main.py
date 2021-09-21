c7630b08b34eb7831173b35712290ee0e14048e4
A Sergey <agibalov.sd@phystech.edu>
2021-09-21 19:18:03 +0300

    1st and 2nd exercises of third contest

--- Contests/main.py
+++ Contests/main.py
@@ -1,10 +1,18 @@
-a = list(i for i in input().split())
-b = set()
-for i in range(len(a)):
-    try:
-        f = open(a[i], 'r')
-    except FileNotFoundError:
-        f = open(a[i], 'w+')
-    finally:
-        b.update(set(f.read().split()))
-print(*sorted(b))
+class Car:
+    def __init__(self, cap, sp, nm):
+        self.capacity = cap
+        self.speed = sp
+        self.number = nm
+
+
+class RaceCar(Car):
+    def __init__(self, sp):
+        super().__init__(0, sp, None)
+
+
+
+c = Car(1000, 100, "a720po")
+print(c.capacity, c.speed, c.number)
+r = RaceCar(300)
+print(r.capacity, r.speed, r.number)
+
