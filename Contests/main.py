fdd322cfe453b0f9e68c869625b66fc35325a21c
A Sergey <agibalov.sd@phystech.edu>
2021-09-28 19:55:07 +0300

    2rd exercise in test 2020 v1

--- Contests/main.py
+++ Contests/main.py
@@ -1,3 +1,2 @@
-numbers = [int(i)for i in input().split()]
-N = int(input())
-print(*sorted(sorted(numbers, reverse=True)[0:N]), sep='\n')
+numbers = [int(i) for i in input().split() if i.lstrip('-+').isdigit()]
+print(sum(numbers))
