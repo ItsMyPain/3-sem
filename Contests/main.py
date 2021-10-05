6f7e6b297563ed06287aa8e3a62f3bbf2ee5e7bd
A Sergey <agibalov.sd@phystech.edu>
2021-10-05 19:25:11 +0300

    3rd exercise in test 2020 v1

--- Contests/main.py
+++ Contests/main.py
@@ -1,2 +1,11 @@
-numbers = [int(i) for i in input().split() if i.lstrip('-+').isdigit()]
-print(sum(numbers))
+def toFixed(f: float, n = 0):
+    a, b = str(f).split('.')
+    return '{}.{}{}'.format(a, b[:n], '0'*(n-len(b)))
+
+number = input()
+ex = number.find('.')
+if ex == -1:
+    print(int(number) - 1)
+else:
+    new1 = (int(float(number) * 10 ** (len(number) - ex - 1)) - 1) / 10 ** (len(number) - ex - 1)
+    print(toFixed(new1, len(number) - ex - 1))
