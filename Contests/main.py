b18a085b6b8ab4fd88b04b0b837d7b03b95473ae
A Sergey <agibalov.sd@phystech.edu>
2021-10-05 19:32:10 +0300

    4rd exercise in test 2020 v1

--- Contests/main.py
+++ Contests/main.py
@@ -1,11 +1,3 @@
-def toFixed(f: float, n = 0):
-    a, b = str(f).split('.')
-    return '{}.{}{}'.format(a, b[:n], '0'*(n-len(b)))
-
-number = input()
-ex = number.find('.')
-if ex == -1:
-    print(int(number) - 1)
-else:
-    new1 = (int(float(number) * 10 ** (len(number) - ex - 1)) - 1) / 10 ** (len(number) - ex - 1)
-    print(toFixed(new1, len(number) - ex - 1))
+input1 = input().split('0')
+out = sorted(input1, key=len, reverse=True)
+print(out)
