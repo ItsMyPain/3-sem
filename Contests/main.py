6fe6e61b945f0a69d21be4697f4525eb4cd5c855
A Sergey <agibalov.sd@phystech.edu>
2021-10-16 22:42:51 +0300

    6rd exercise in test 2020 v1

--- Contests/main.py
+++ Contests/main.py
@@ -1,5 +1,29 @@
-import pandas as pd
-st = [[int(i) for i in input().split()] for i in range(int(input()))]
-df = pd.DataFrame(st, columns=list('XYL'))
-df1 = df[(df['L'] >= 10) & (df['X']**2 + df['Y']**2 <= 100)]
-print(df1)
+class GasStation:
+    val = 0
+    cap = 0
+
+    def __init__(self, c):
+        self.cap = c
+
+    # Конструктор, принимающий один параметр - ёмкость резервуара колонки
+    # Резервуар создаётся пустой
+
+    # Залить в резервуар колонки n литров топлива
+    # Если столько не влезает в резервуар - не заливать ничего, выбросить exception
+    def fill(self, n):
+        if self.val + n <= self.cap:
+            self.val += n
+        else:
+            raise Exception
+
+    # Заправиться, забрав при этом из резервура n литров топлива
+    # Если столько нет в резервуаре - не забирать из резервуара ничего, выбросить exception
+    def tank(self, n):
+        if self.val - n >= 0:
+            self.val -= n
+        else:
+            raise Exception
+
+    # Запросить остаток топлива в резервуаре
+    def get_limit(self):
+        return self.val
