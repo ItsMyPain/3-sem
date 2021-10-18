1f29950e0d051958dc4612eae8b31cea1a7752f2
A Sergey <agibalov.sd@phystech.edu>
2021-10-18 20:34:17 +0300

    7rd exercise in test 2020 v1

--- Contests/main.py
+++ Contests/main.py
@@ -1,29 +1,16 @@
-class GasStation:
-    val = 0
-    cap = 0
+data = set()
+data_low = dict()
+for i in range(int(input())):
+    inp = input().split()
+    for j in inp:
+        data.add(j)
+        data_low[j.lower()] = 0
 
-    def __init__(self, c):
-        self.cap = c
+for i in data_low:
+    for j in data:
+        if i == j.lower():
+            data_low[j.lower()] += 1
 
-    # Конструктор, принимающий один параметр - ёмкость резервуара колонки
-    # Резервуар создаётся пустой
-
-    # Залить в резервуар колонки n литров топлива
-    # Если столько не влезает в резервуар - не заливать ничего, выбросить exception
-    def fill(self, n):
-        if self.val + n <= self.cap:
-            self.val += n
-        else:
-            raise Exception
-
-    # Заправиться, забрав при этом из резервура n литров топлива
-    # Если столько нет в резервуаре - не забирать из резервуара ничего, выбросить exception
-    def tank(self, n):
-        if self.val - n >= 0:
-            self.val -= n
-        else:
-            raise Exception
-
-    # Запросить остаток топлива в резервуаре
-    def get_limit(self):
-        return self.val
+for key, val in sorted(data_low.items(), key=lambda x: [-x[1], x[0]]):
+    if val > 2:
+        print(key, end='\n')
