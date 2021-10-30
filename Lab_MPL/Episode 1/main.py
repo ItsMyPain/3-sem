ebbf0728e4507b525b1d61242716e50831175e0f
A Sergey <agibalov.sd@phystech.edu>
2021-10-30 15:58:33 +0300

    Added close

--- Lab_MPL/Episode 1/main.py
+++ Lab_MPL/Episode 1/main.py
@@ -11,11 +11,9 @@ for num in range(1, 6):
         x.append(float(r[0]))
         y.append(float(r[1]))
 
-    plt.plot(x, y, ".C%d" % num, label="00%d.dat" % num)
+    f.close()
 
+    plt.plot(x, y, ".C%d" % num, label="00%d.dat" % num)
     plt.legend()
     plt.savefig("../00%d.png" % num)
     plt.show()
-
-# Ща брат уйдет с батей я включусь
-# Ахаха,ок
