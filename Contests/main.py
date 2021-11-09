7f77df69799999f9e722b3831a7a96762454eea6
A Sergey <agibalov.sd@phystech.edu>
2021-11-09 19:28:15 +0300

    2rd exercise of test NumPy

--- Contests/main.py
+++ Contests/main.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 N, M = map(int, input().split())
-K = int(input())
-st = np.ones(shape=(N, M))
 
-for i in range(K):
-    x, y, r = map(int, input().split())
-    st[max(0, x - r):min(x + r + 1, N), max(0, y - r):min(y + r + 1, M)] = 0
+st = np.zeros(shape=(N, M), dtype=np.int32)
+for i in range(N):
+    st[i:] = np.fromstring(input(), sep=' ')
 
-print(int(st.sum()))
+print(st[st < -5].size)
+print(- st[st < 0].sum())
+print(st.max())
