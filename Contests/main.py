da25982639fa318c51b4f49b25561d8607b773f8
A Sergey <agibalov.sd@phystech.edu>
2021-11-16 19:53:30 +0300

    1rd exercise of test Pandas

--- Contests/main.py
+++ Contests/main.py
@@ -1,7 +1,19 @@
-import numpy as np
+import pandas as pd
 
-a = np.loadtxt(input())
-b = np.loadtxt(input())
-x = np.fromstring(input(), sep=' ')
+g_path = input()#'games001.csv'
+r_path = input()#'rates001.csv'
 
-print(a @ a @ x @ b)
+games = pd.read_csv(g_path, sep=';')
+rates = pd.read_csv(r_path, sep=';')
+
+rates_mean = rates.groupby('id').mean()
+
+data = games.merge(rates_mean, left_on='id', right_index=True)
+res1 = data.sort_values(by='mark', ascending=False).head(3)[['name', 'mark']]
+
+for v in res1.values:
+    print(f'{v[0]} {v[1]:.3f}')
+
+res2 = data[data['mark'] > 8.0].loc[:, ['company', 'mark']].groupby('company').count() \
+    .sort_values(by='mark', ascending=False).head(1)
+print(res2.index[0], res2.iloc[0].values[0])
