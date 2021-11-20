53efff16994f0ce2898ff506f01f12996a7910a2
A Sergey <agibalov.sd@phystech.edu>
2021-11-20 23:12:21 +0300

    Episode 1 completed (v 2.0)

--- Lab_Pandas/episode1/main.py
+++ Lab_Pandas/episode1/main.py
@@ -3,4 +3,4 @@ import pandas as pd
 Data = pd.read_csv('transactions.csv', sep=',')
 print(Data[Data['STATUS'] == 'OK'].sort_values(by='SUM', ascending=False).head(3))
 RealData = Data[Data['STATUS'] == 'OK']
-print(RealData[RealData['CONTRACTOR'] == 'Umbrella, Inc']['SUM'].sum())
+print('Amount of Umbrella =', RealData[RealData['CONTRACTOR'] == 'Umbrella, Inc']['SUM'].sum())
