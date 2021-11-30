cb193cd74aece26d28914f00c01e18849dd1b5cf
A Sergey <agibalov.sd@phystech.edu>
2021-11-30 20:04:18 +0300

    Issues 3 completed

--- Lab_MPL/Episode 2/main.py
+++ Lab_MPL/Episode 2/main.py
@@ -5,6 +5,7 @@ from PIL import Image
 n = 0
 m = 0
 fig, axs = plt.subplots(3, 2, figsize=[12, 12])
+fig1, axs1 = plt.subplots()
 
 with open("01.dat") as file:
     while True:
@@ -25,33 +26,23 @@ with open("01.dat") as file:
             m += 1
         else:
             n += 1
-plt.subplots_adjust(wspace=0.3, hspace=0.4)
-plt.savefig("res.png")
+
+        axs1.plot(x, y)
+        axs1.set_title('Frame %d' % (1 + n + 3 * m))
+        axs1.set_xlim([0, 16])
+        axs1.set_ylim([-12, 12])
+        axs1.grid()
+        axs1.yaxis.set_major_locator(ticker.MultipleLocator(2))
+
+        fig1.savefig(f'GIF/res{(1 + n + 3 * m)}.png')
+        axs1.clear()
+
+fig.subplots_adjust(wspace=0.3, hspace=0.4)
+fig.savefig("res.png")
 plt.close(fig)
 # plt.show()
 # ------------------------------------------------------ #
 
-
-num = 0
-with open("01.dat") as file:
-    while True:
-        fig, axs = plt.subplots()
-        num += 1
-        line1 = file.readline()
-        line2 = file.readline()
-        if not line2:
-            break
-        x = [float(i) for i in line1.split()]
-        y = [float(i) for i in line2.split()]
-        axs.plot(x, y)
-        axs.set_title('Frame %d' % num)
-        axs.set_xlim([0, 16])
-        axs.set_ylim([-12, 12])
-        axs.grid()
-        axs.yaxis.set_major_locator(ticker.MultipleLocator(2))
-
-        plt.savefig(f'GIF/res{num}.png')
-        plt.close(fig)
 
 frames = []
 
