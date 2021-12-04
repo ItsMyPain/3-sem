941acd2b882987f8017579afd5b8aff9a649e452
A Sergey <agibalov.sd@phystech.edu>
2021-12-05 01:49:24 +0300

    1 episode complited

---
+++ Lab_Scipy/Episode1/main.py
@@ -0,0 +1,16 @@
+import sympy as sym
+
+ro, lyambda, mu = sym.symbols('ro, l, m')
+matrix = sym.Matrix.zeros(9, 9)
+
+matrix[0, 3] = -1 / ro
+matrix[1, 4] = -1 / ro
+matrix[2, 5] = -1 / ro
+matrix[3, 0] = -(lyambda + 2 * mu)
+matrix[6, 0] = -lyambda
+matrix[8, 0] = -lyambda
+matrix[4, 1] = -mu
+matrix[5, 2] = -mu
+matrix = sym.Matrix(matrix)
+
+print(*matrix.eigenvals().items(), sep='\n')
