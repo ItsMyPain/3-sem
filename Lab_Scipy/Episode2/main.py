bd7016d7865ee27579e113298076e0514cb585e0
A Sergey <agibalov.sd@phystech.edu>
2021-12-05 02:15:25 +0300

    episode 2 add

--- Lab_Scipy/Episode2/main.py
+++ Lab_Scipy/Episode2/main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+import numpy as np
+from scipy import linalg
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+with open('large.txt') as f:
+    data = f.readlines()
+    for i in range(len(data)):
+        data[i] = data[i].split()
 
+n = int(data[0][0])
+a = np.array(data[1:n+1], dtype=np.float64)
+b = np.array(data[n+1])
+x = linalg.solve(a, b)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+fig, ax = plt.subplots()
+ax.bar(np.arange(1, n+1), x)
+ax.set_title('Решение СЛАУ')
+ax.grid(True)
+plt.savefig('res.png')
+plt.show()
