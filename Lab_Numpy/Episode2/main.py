656bbf35c807528c9838b345bde9fcbbf9f8fe15
A Sergey <agibalov.sd@phystech.edu>
2021-12-14 15:37:46 +0300

    issues completed

--- Lab_Numpy/Episode2/main.py
+++ Lab_Numpy/Episode2/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def slide_average(N):
+def tenelem(N):
     global data
     return data[max(0, N - 9): N + 1].mean()
 
@@ -10,7 +10,7 @@ def slide_average(N):
 for i in range(1, 4):
     data = np.genfromtxt('signal0' + str(i) + '.dat')
     data1 = np.convolve(data, np.ones(10) / 10, mode='valid')
-    data2 = [*map(slide_average, [0, 1, 2, 3, 4, 5, 6, 7, 8])]
+    data2 = [*map(tenelem, [0, 1, 2, 3, 4, 5, 6, 7, 8])]
     res = np.concatenate((data2, data1))
 
     plt.subplot(1, 2, 1)
